a6ffa76e1ffa52d75a1fe8fc607f6f54b0105fda
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2024-07-19 15:04:32 -0700

    Time column fix

--- isp_compare/ping.ps1
+++ isp_compare/ping.ps1
@@ -61,7 +61,7 @@ function Out-File
 
     if( -not (Test-Path $path) )
     {
-        $header = "Timestamp,Value"
+        $header = "Time,Value"
         $header | Add-Content -Path $path
     }
 
